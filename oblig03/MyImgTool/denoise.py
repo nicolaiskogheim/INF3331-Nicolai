3ada9ae6544c70cba2b387452d52cdde20a0a90d
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2014-11-11 15:10:21 +0100

    Adds ass. 1 vanilla python

---
+++ oblig03/MyImgTool/denoise.py
@@ -0,0 +1,48 @@
+from PIL import Image
+import cProfile
+import pstats
+import argparse
+
+def img2list(imgPath):
+    im = Image.open(imgPath)
+    data = list(im.getdata())
+    w, h = im.size
+    return data,h,w
+
+def list2img(data,h,w,imgPath):
+    im = Image.new("L", (w, h))
+    im.putdata(data)
+    im.save(imgPath)
+
+def denoise(data,h,w, kappa=0.1, iter=10):
+    for _ in xrange(0,iter):
+        for i in xrange(1, w-1): 
+            for j in xrange(1, h-1):
+                data[j*w+i] += \
+                              kappa*(data[(j-1)*w+i]
+                                   + data[j*w+(i-1)]
+                                   - 4*data[j * w + i]
+                                   + data[j*w+(i+1)]
+                                   + data[i+ w * (j+1)]
+                                   )
+
+    # data1[i*b+j] = data[i*b+j] +kappa*(data[(i-1)*b+j]+data[i*b+(j-1)] -4*data[i*b+j] +data[i*b+(j+1)]+data[(i+1)*b+j])
+    return data
+
+if __name__=="__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("imgPathIn", help="Path to source image")
+    parser.add_argument("imgPathOut", help="Path to resulting image")
+    parser.add_argument("-k","--kappa", default=0.1,
+                        help="Value between 0 and 1.")
+    parser.add_argument("-i","--iter", default=10,
+                        help="Times do do denoising")
+
+    args = parser.parse_args();
+
+    imgList, h, w = img2list(args.imgPathIn)
+    
+    data = denoise(imgList,h,w, args.kappa, args.iter)
+
+    list2img(data,h,w,args.imgPathOut)
