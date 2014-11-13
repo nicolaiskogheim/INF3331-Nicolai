1c65b6d15550993d8cfa4050a0fbdc2fcbddd7aa
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2014-11-13 12:00:34 +0100

    Adds ass. 2 profiling

--- oblig03/MyImgTool/profiling.py
+++ oblig03/MyImgTool/profiling.py
@@ -1,8 +1,22 @@
-from denoise import denoise as python_denoise
-from denoise_weave import denoise as weave_denoise
+import cProfile
+import pstats
+from denoise import run  as python_denoise
+from denoise_weave import run as weave_denoise
+
+image_in_path="disasterbefore.jpg"
+image_out_python="disasterafter.python.jpg"
+image_out_weave="disasterafter.weave.jpg"
+
+kappa=0.1
+iter=10
 
 if __name__=="__main__":
+    cProfile.run("python_denoise(image_in_path,image_out_python,kappa,iter)", "python_timing")
+    cProfile.run("weave_denoise(image_in_path,image_out_python,kappa,iter)", "weave_timing")
 
+    stats = pstats.Stats("python_timing")
+    stats.add("weave_timing")
 
-    # print cProfile.run("denoise(imgList,h,w,args.kappa,args.iter)", "numpy_timing")
-    # pstats.Stats("numpy_timing").sort_stats("cumulative").print_stats()
+    stats.strip_dirs()
+    stats.sort_stats("cumulative")
+    stats.print_stats()
