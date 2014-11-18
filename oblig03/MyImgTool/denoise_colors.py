24f331464b67d24f75a7bc55f18a22af19f7b11c
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2014-11-18 15:28:28 +0100

    Adds hsi2rgb function

--- oblig03/MyImgTool/denoise_colors.py
+++ oblig03/MyImgTool/denoise_colors.py
@@ -46,6 +46,90 @@ def rgb2hsi(data):
     hsi = np.dstack((h,s,i))
     return hsi
 
+def hsi2rgb(data):
+    h,s,i = data[:,:,0], data[:,:,1], data[:,:,2]
+    r,g,b = np.copy(h),np.copy(s),np.copy(i)
+
+    """
+    In case the reader is not familar with numpy masks,
+    here is an explanation.
+    For every element in `r` and `h` (in the code below)
+    if the `h` element equals 0, evaluate the expression
+    on the right of the equals sign.
+    So in this first code block, every element of `r`
+    where the same index in `h` equals 0, the element
+    in `r` will get new values.
+    The other arrays (`g`,`b`,`i` and `s`) work the same
+    since the mask is applied.
+    The arrays need to have the same size and shape for
+    this to work.
+    """
+    mask = h == 0
+    r[mask], g[mask], b[mask] = \
+            i[mask] + 2*i[mask]*s[mask], \
+            i[mask] - i[mask]*s[mask], \
+            i[mask] - i[mask]*s[mask], \
+
+    m = (0 < h) & (h < 120)
+    r[m], g[m], b[m] = \
+            i[m]+i[m]*s[m]*(np.cos(np.radians(h[m]))/np.cos(np.radians(60 - h[m]))),\
+            i[m]+i[m]*s[m]*(1-(np.cos(np.radians(h[m]))/np.cos(np.radians(60 - h[m])))),\
+            i[m]-i[m]*s[m] 
+
+    m = h == 120
+    r[m], g[m], b[m] = \
+            i[m] - i[m]*s[m],\
+            i[m] + 2*i[m]*s[m],\
+            i[m] - i[m]*s[m]
+
+    m = (120 < h) & (h < 240)
+    r[m], g[m], b[m] = \
+            i[m] - i[m]*s[m],\
+            i[m]+i[m]*s[m]*(np.cos(np.radians(h[m] - 120))/np.cos(np.radians(180 - h[m]))),\
+            i[m] + i[m]*s[m]*(1 - (np.cos(np.radians(h[m] - 120))/np.cos(np.radians(180 - h[m]))))
+
+    m = h == 240
+    r[m], g[m], b[m] = \
+            i[m] - i[m] * s[m],\
+            i[m] - i[m] * s[m],\
+            i[m] + 2*i[m]*s[m]
+
+    m = (240 < h) & (h <= 360) 
+    r[m], g[m], b[m] = \
+            i[m] + i[m]*s[m]*(1 - np.cos(np.radians(h[m] - 240))/np.cos(np.radians(300 - h[m]))),\
+            i[m] - i[m]*s[m],\
+            i[m] + i[m]*s[m]*(np.cos(np.radians(h[m] - 240))/np.cos(np.radians(300 - h[m])))
+
+    return np.dstack((r,g,b))
+
+def denoise(data, kappa=0.1, iter=10):
+    h,w = data.shape[:2]
+    data_new = data.copy()
+    tmp = data.copy()
+    in_vars = ["data","data_new","tmp","kappa", "iter", "h", "w"]
+    code=r"""
+        for (int round=0; round<iter; round++)
+        {
+            for (int i=1; i<h-1; i++)
+            {
+                for (int j=1; j<w-1; j++)
+                {
+                    data_new(i,j) = data(i,j) + kappa*(data(i-1,j)
+                    +data(i,j-1) -4*data(i,j) +data(i,j+1)
+                    +data(i+1,j));
+                }
+            }
+            tmp = data;
+            data = data_new;
+            data_new = tmp;
+        }
+    """
+   
+    comp=weave.inline(code,
+                      in_vars,
+                      type_converters=weave.converters.blitz) 
+    return data
+
 
 
 if __name__=="__main__":
