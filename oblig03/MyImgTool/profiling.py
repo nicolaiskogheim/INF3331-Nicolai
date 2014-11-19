b2b2556986d8fc8340b39702f0dc1da476d09799
Nicolai Skogheim <nicolai.skogheim@gmail.com>
2014-11-19 01:39:38 +0100

    Adds profiling of color backend

--- oblig03/MyImgTool/profiling.py
+++ oblig03/MyImgTool/profiling.py
@@ -2,20 +2,25 @@ import cProfile
 import pstats
 from denoise import run  as python_denoise
 from denoise_weave import run as weave_denoise
+from denoise_colors import run as color_denoise
 
-image_in_path="disasterbefore.jpg"
-image_out_python="disasterafter.python.jpg"
-image_out_weave="disasterafter.weave.jpg"
+image_in_bnw="disasterbefore.jpg"
+image_in_color="disastercolor.jpg"
+image_out_python="disasterafter.python.profiling.jpg"
+image_out_weave="disasterafter.weave.profiling.jpg"
+image_out_color="disasterafter.color.profiling.jpg"
 
 kappa=0.1
 iter=10
 
 if __name__=="__main__":
-    cProfile.run("python_denoise(image_in_path,image_out_python,kappa,iter)", "python_timing")
-    cProfile.run("weave_denoise(image_in_path,image_out_weave,kappa,iter)", "weave_timing")
+    cProfile.run("python_denoise(image_in_bnw,image_out_python,True,kappa,iter)", "python_timing")
+    cProfile.run("weave_denoise(image_in_bnw,image_out_weave,True,kappa,iter)", "weave_timing")
+    cProfile.run("color_denoise(image_in_color,image_out_color,True,kappa,iter,red=10,hue=10)", "color_timing")
 
     stats = pstats.Stats("python_timing")
     stats.add("weave_timing")
+    stats.add("color_timing")
 
     stats.strip_dirs()
     stats.sort_stats("cumulative")
